612d101c7b9a4667f3761dfe8e45d0bbadc0de2d
Eric Fry <yuv422@users.noreply.github.com>
2018-07-21 21:51:14 +1000

    Added support for comment command Cmd + '/'

---
+++ src/com/nasmlanguage/NASMCommenter.java
@@ -0,0 +1,56 @@
+/*++
+
+NASM Assembly Language Plugin
+Copyright (c) 2017-2018 Aidan Khoury. All rights reserved.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+--*/
+
+package com.nasmlanguage;
+
+import com.intellij.lang.Commenter;
+import org.jetbrains.annotations.Nullable;
+
+public class NASMCommenter implements Commenter {
+    @Nullable
+    @Override
+    public String getLineCommentPrefix() {
+        return ";";
+    }
+
+    @Nullable
+    @Override
+    public String getBlockCommentPrefix() {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getBlockCommentSuffix() {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getCommentedBlockCommentPrefix() {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getCommentedBlockCommentSuffix() {
+        return null;
+    }
+}
