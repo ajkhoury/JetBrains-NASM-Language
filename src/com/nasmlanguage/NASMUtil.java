3b8541c3183e7a9688e4b89cc658b355bf009a32
ajkhoury <aidankhoury@gmail.com>
2019-01-09 15:53:53 -0400

    Got rid of unused code.

--- src/com/nasmlanguage/NASMUtil.java
+++ src/com/nasmlanguage/NASMUtil.java
@@ -26,7 +26,6 @@ SOFTWARE.
 package com.nasmlanguage;
 
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.vfs.LocalFileSystem;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
@@ -39,54 +38,6 @@ import com.nasmlanguage.psi.*;
 import java.util.*;
 
 class NASMUtil {
-
-    static List<PsiElement> findPreprocessorMacrosAndDefines(Project project) {
-        List<PsiElement> result = new ArrayList<>();
-        Collection<VirtualFile> virtualFiles = FileBasedIndex.getInstance().getContainingFiles(
-                FileTypeIndex.NAME, NASMFileType.INSTANCE, GlobalSearchScope.allScope(project)
-        );
-        for (VirtualFile virtualFile : virtualFiles) {
-            NASMFile assemblyFile = (NASMFile)PsiManager.getInstance(project).findFile(virtualFile);
-            if (assemblyFile != null) {
-                Collection<NASMPreprocessor> nasmPreprocessors = PsiTreeUtil.collectElementsOfType(assemblyFile, NASMPreprocessor.class);
-                if (!nasmPreprocessors.isEmpty()) {
-                    for (NASMPreprocessor nasmPreprocessor : nasmPreprocessors) {
-                        NASMMacro macro = nasmPreprocessor.getMacro();
-                        if (macro != null) {
-                            result.add(macro);
-                            continue;
-                        }
-                        NASMDefine define = nasmPreprocessor.getDefine();
-                        if (define != null) {
-                            result.add(define);
-                        }
-                    }
-                }
-            }
-        }
-        return result;
-    }
-
-    static List<NASMMacro> findPreprocessorMacros(Project project) {
-        List<NASMMacro> result = new ArrayList<>();
-        Collection<VirtualFile> virtualFiles = FileBasedIndex.getInstance().getContainingFiles(
-            FileTypeIndex.NAME, NASMFileType.INSTANCE, GlobalSearchScope.allScope(project)
-        );
-        for (VirtualFile virtualFile : virtualFiles) {
-            NASMFile assemblyFile = (NASMFile)PsiManager.getInstance(project).findFile(virtualFile);
-            if (assemblyFile != null) {
-                Collection<NASMPreprocessor> nasmPreprocessors = PsiTreeUtil.collectElementsOfType(assemblyFile, NASMPreprocessor.class);
-                if (!nasmPreprocessors.isEmpty()) {
-                    for (NASMPreprocessor nasmPreprocessor : nasmPreprocessors) {
-                        NASMMacro macro = nasmPreprocessor.getMacro();
-                        if (macro != null)
-                            result.add(macro);
-                    }
-                }
-            }
-        }
-        return result;
-    }
 
     static List<NASMDefine> findPreprocessorDefines(PsiFile containingFile) {
         List<NASMDefine> result = new ArrayList<>();
